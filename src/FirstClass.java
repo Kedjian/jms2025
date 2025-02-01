af7c78e54d060d16fc8d1fc2daafd8d14e620f25
Kedjian <o.mat.makowski@gmail.com>
2025-02-01 03:24:34 +0100

    Working through the course

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -20,9 +20,35 @@ public class FirstClass {
         else
             System.out.println("You didn't get the high score!");
 
-        int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100) {
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less than 100!");
         }
+        if ((topScore > secondTopScore) || (topScore < 100)) {
+            System.out.println("At least one is true. Greater than second top score or less than 100!");
+        }
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is not an error.");
+        }
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("I'm car, and this is not supposed to happen.");
+        }
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country.");
+        }
+        else {
+            System.out.println("This car is not domestic to our country.");
+        }
+        int ageOfClient = 16;
+        String ageText = (ageOfClient >= 18) ? "over eighteen" : "still a kid";
+        System.out.println("Our client is " + ageText + ".");
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
     }
 }
