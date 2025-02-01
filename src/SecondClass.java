29e912c35fbdccea5382b21adf4c0e51ee61974b
Kedjian <o.mat.makowski@gmail.com>
2025-02-01 03:40:08 +0100

    Working through the course

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,2 +1,16 @@
 public class SecondClass {
+
+    public static void main(String[] args) {
+        double num1 = 20.00d;
+        double num2 = 80.00d;
+        double sum1sl = (num1 + num2) * 100.00d;
+        System.out.println("TOTAL: " + sum1sl);
+        double mod1sl = sum1sl % 40.00d;
+        System.out.println("REMAINDER: " + mod1sl);
+        boolean isZero = (mod1sl == 0) ? true : false;
+        if (!isZero) {
+            System.out.println("GOT REMAINDER.");
+        }
+        System.out.println("BOOLEAN IS EQUAL TO: " + isZero + ".");
+    }
 }
