f7c6a81d41c8091d8196269bf24cddd58ffde6a1
Kedjian <o.mat.makowski@gmail.com>
2025-02-02 03:10:59 +0100

    Working through the course

--- src/methodsexercise.java
+++ src/methodsexercise.java
@@ -1,27 +1,35 @@
 public class methodsexercise {
     public static void main(String[] args) {
+
         // Java's description of the method is: a declared executable code
         // that can be invoked, passing a fixed number of values as arguments.
-        calculateScore(true, 1000, 8, 500);
-        calculateScore(true, 8100, 16, 340);
+
+        int highScore = calculateScore(true, 1000, 8, 500); System.out.println(highScore);
+        highScore = calculateScore(true, 8100, 16, 340); System.out.println(highScore);
+
         simpleCalculator(2, 6, 4, 2);
         simpleCalculator(3, 6, 8, 1);
+
+        System.out.println(calculateMyAge(2001));
+        System.out.println(calculateMyAge(2007));
     }
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         int finalScore = score;
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score is: " + finalScore + ".");
         }
+        return finalScore;
     }
     public static void simpleCalculator(double a, double b, double c, double d) {
-        double sum = a + b + c + d;
-        double sub = a - b - c - d;
-        double mul = a * b * c * d;
-        double div = a / b / c / d;
-        System.out.println("Sum of " + a + " and " + b + " and " + c + " and " + d + " = " + sum + ".");
-        System.out.println("Subtraction of " + a + " and " + b + " and " + c + " and " + d + " = " + sub + ".");
-        System.out.println("Multiplication of " + a + " and " + b + " and " + c + " and " + d + " = " + mul + ".");
-        System.out.println("Division of " + a + " and " + b + " and " + c + " and " + d + " = " + div + ".");
+
+        double sum = a + b + c + d; System.out.println(sum);
+        double sub = a - b - c - d; System.out.println(sub);
+        double mul = a * b * c * d; System.out.println(mul);
+        double div = a / b / c / d; System.out.println(div);
+    }
+    public static int calculateMyAge(int dateOfBirth) {
+
+        return (2025 - dateOfBirth);
     }
 }
