78680158becb8d966b3c6ca958505becdc245352
Kedjian <o.mat.makowski@gmail.com>
2025-02-04 00:35:39 +0100

    added enhanced switch upgrade

--- src/controlflowexercise.java
+++ src/controlflowexercise.java
@@ -2,18 +2,13 @@ public class controlflowexercise {
     public static void main(String[] args) {
         int switchValue = 5;
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1.");
-                break;
-            case 2:
-                System.out.println("Value was 2.");
-                break;
-            case 3: case 4: case 5: case 6: case 7: case 8:
+            case 1 -> System.out.println("Value was 1.");
+            case 2 -> System.out.println("Value was 2.");
+            case 3, 4, 5, 6, 7, 8 -> {
                 System.out.println("Value was 3, 4, 5, 6, 7 or 8.");
                 System.out.println("Actually it was a " + switchValue + ".");
-                break;
-            default:
-                System.out.println("Value was not 1, 2, 3, 4, 5, 6, 7 or 8.");
+            }
+            default -> System.out.println("Value was not 1, 2, 3, 4, 5, 6, 7 or 8.");
         }
     }
 }
