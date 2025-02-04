8f69640746db1ae0a2e2b2b678d215cd779624f1
Kedjian <o.mat.makowski@gmail.com>
2025-02-04 01:04:08 +0100

    added getQuarter method

--- src/controlflowexercise.java
+++ src/controlflowexercise.java
@@ -1,14 +1,18 @@
 public class controlflowexercise {
     public static void main(String[] args) {
-        int switchValue = 5;
-        switch (switchValue) {
-            case 1 -> System.out.println("Value was 1.");
-            case 2 -> System.out.println("Value was 2.");
-            case 3, 4, 5, 6, 7, 8 -> {
-                System.out.println("Value was 3, 4, 5, 6, 7 or 8.");
-                System.out.println("Actually it was a " + switchValue + ".");
-            }
-            default -> System.out.println("Value was not 1, 2, 3, 4, 5, 6, 7 or 8.");
-        }
+        String month = "JANUARY"; System.out.println(getQuarter(month));
+        month = "JULY"; System.out.println(getQuarter(month));
+        month = "Not a month"; System.out.println(getQuarter(month));
+
+    }
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> month + " is not valid.";
+        };
     }
 }
+
