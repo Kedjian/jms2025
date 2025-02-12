f39049b29c0a2d65833bd2b2af2bf646a17a332c
Kedjian <o.mat.makowski@gmail.com>
2025-02-12 02:26:21 +0100

    added canPack method and its executive

--- src/controlflowexercise.java
+++ src/controlflowexercise.java
@@ -28,6 +28,7 @@ public class controlflowexercise {
         getGreatestCommonDivisor(25,15);
         isPerfectMethod(6);
         numberToWords(23);
+        canPack(1,0,4);
 
         int startNum = 5;
         int endNum = 20;
@@ -396,6 +397,29 @@ public class controlflowexercise {
         for (int i = 1; i <= num; i *= 10) {
             count++;
         } return count;
+    }
+
+    private static final int BIG_WEIGHT = 5;
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+
+        boolean result = false;
+        int totalBigWeight = bigCount * BIG_WEIGHT;
+        if (totalBigWeight >= goal) {
+            int remaining = goal % BIG_WEIGHT;
+            if (smallCount >= remaining) {
+                result = true;
+            }
+        } else {
+            if (smallCount >= goal - totalBigWeight) {
+                result = true;
+            }
+        }
+        return result;
     }
 }
 
