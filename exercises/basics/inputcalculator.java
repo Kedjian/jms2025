a0463c0d3275b437938435accd48acb20547045d
Kedjian <o.mat.makowski@gmail.com>
2025-02-13 13:32:12 +0100

    moving to the archives

---
+++ exercises/basics/inputcalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class inputcalculator {
+    public static void main(String[] args) {
+
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage() {
+        int num;
+        int sum = 0;
+        int rep = 0;
+        long avg = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            if (sc.hasNextInt()) {
+                num = sc.nextInt();
+                sum += num;
+                rep++;
+            } else {
+                avg = (long) round(avg, rep);
+                break;
+            }
+        } while (true);
+
+        System.out.print("SUM = " + sum + " ");
+        System.out.print("AVG = " + avg);
+    }
+
+    public static double round(double value, int rep) {
+        value = Math.round((double) value / rep);
+        return value;
+    }
+}
