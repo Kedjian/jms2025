2f9366252cf9ea756fcf2366b23ffbf5f590635d
Kedjian <o.mat.makowski@gmail.com>
2025-02-13 17:35:42 +0100

    first steps in oop

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         Car car1 = new Car();
+        car1.setColor("Red");
+        car1.setMake("Burger");
+        car1.setModel("Samsung");
+        car1.setConvertible(false);
+        car1.setDoors(6);
         car1.describeCar();
     }
 }
