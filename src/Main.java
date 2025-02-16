1dcf0e79acef1bb0a2e923c1342c1db967b04256
Kedjian <o.mat.makowski@gmail.com>
2025-02-16 17:02:11 +0100

    added Point.java class and pointTest method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,16 @@ public class Main {
     public static void main(String[] args) {
     }
 
+    public static void pointTest() {
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+    }
+
     public static void wallTest() {
         Wall test = new Wall();
         System.out.println(test.getArea());
