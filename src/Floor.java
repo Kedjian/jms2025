1a322625dd9d3623d6127733106daa5cdb1a75da
Kedjian <o.mat.makowski@gmail.com>
2025-02-16 17:12:18 +0100

    added three classes: Calculator.java, Carpet.java, Floor.java and one method costCalculatorTest

---
+++ src/Floor.java
@@ -0,0 +1,22 @@
+public class Floor {
+    private double width;
+    private double height;
+
+    public Floor(double width, double height) {
+        if (width <= 0) {
+            width = 0;
+        }
+        if (height <= 0) {
+            height = 0;
+        }
+
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+
+}
