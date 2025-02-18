0299a8793e2b6ca3ef73098fefcb20396dfd4775
Kedjian <o.mat.makowski@gmail.com>
2025-02-18 03:56:29 +0100

    added Animal.java class, Cat.java subclass, Dog.java subclass and a doAnimalStuff method

---
+++ src/Cat.java
@@ -0,0 +1,6 @@
+public class Cat extends Animal {
+
+    public Cat() {
+        super("The ugly kind of a bald cat", "fat as fuck", 5);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
 
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+
+        Cat cat = new Cat();
+        doAnimalStuff(cat, "slow");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
     }
 
     public static void trafficLightTest() {
