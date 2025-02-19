a68cb07f321fcfcb97665e5561503380864a1dfc
Kedjian <o.mat.makowski@gmail.com>
2025-02-19 14:37:19 +0100

    added Rectangle.java class, Cuboid.java subclass and rectangleTest method

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,19 @@ public class Main {
 
     }
 
+    public static void rectangleTest() {
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+    }
+
     public static void circleTest() {
         Circle circle = new Circle(3.75);
         System.out.println("circle.radius= " + circle.getRadius());
