6583c7ee4b369e9aba09ef9113ab0f58cfcb234a
Kedjian <o.mat.makowski@gmail.com>
2025-02-19 14:13:38 +0100

    added Circle.java class, Cylinder.java subclass and circleTest method

---
+++ src/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+    protected double radius;
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,17 @@ public class Main {
 
     }
 
+    public static void circleTest() {
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
+    }
+
     public static void stringFormattingTest() {
         String bulletId = "Print a bulleted list:\n" +
                 "\t\u2022 First point\n" +
