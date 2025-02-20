878d073622205e34bd1fca1f5778c4a1d76ac0db
Kedjian <o.mat.makowski@gmail.com>
2025-02-20 02:37:02 +0100

    created computerBuildTest method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static void computerBuildTest() {
         ComputerCase theCase = new ComputerCase("2208", "Dell", "240");
         Monitor theMonitor = new Monitor("27inch Beast","Acer", 27, "2540 x 1440");
         Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
