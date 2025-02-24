a9ff6a7c66b2c7950112cad4b45538fb00e9adb0
Kedjian <o.mat.makowski@gmail.com>
2025-02-24 23:47:32 +0100

    finished encapsulation studies

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ public class Main {
 
     public static void printerTest() {
         Printer printer = new Printer(50, true);
-        System.out.println("Initial page count = " + printer.getPagesPrinted());
-
-        int pagesPrinted = printer.printPages(5);
-        System.out.println("Printed pages = " + printer.getPagesPrinted());
+        System.out.println(printer.addToner(50));
+        System.out.println("Initial page count = " +printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
     }
 
     public static  void playerTest() {
