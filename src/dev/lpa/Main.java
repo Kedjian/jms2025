844d55839ec65128728197d25f42b36d5e7222d6
Kedjian <o.mat.makowski@gmail.com>
2025-03-09 23:46:05 +0100

    studying the difference between reference and value types

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,6 +5,25 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println(Arrays.toString(anotherArray));
+        System.out.println(Arrays.toString(myIntArray));
+
+        anotherArray[0] = 1;
+        modifyArray(myIntArray);
+
+        System.out.println(Arrays.toString(anotherArray));
+        System.out.println(Arrays.toString(myIntArray));
+
+        //arrays can refer each other. anotherArray holds reference to myIntArray, and although it looks like
+        //we have two arrays, that's simply not true. we only have one array in memory there, the myIntArray. it just can be
+        //accessed by anotherArray as well.
+    }
+
+    private static void modifyArray(int[] array) {
+        array[1] = 2;
     }
 
     private static void arrayRecap() {
