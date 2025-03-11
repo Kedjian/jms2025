7a5348079b3a16c7bdb8119a3c9fd128b0164d94
Kedjian <o.mat.makowski@gmail.com>
2025-03-11 18:36:08 +0100

    array studies

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,10 +1,64 @@
 package dev.lpa;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+
+        int returnedMin = findMin(returnedArray);
+        System.out.println(returnedMin);
+    }
+
+    private static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a list of integers, separated by commas:");
+        String input = scanner.nextLine();
+
+        String[] splits = input.split(",");
+        int[] values = new int[splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            values[i] = Integer.parseInt(splits[i].trim());
+        }
+
+        return values;
+    }
+
+    private static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int el : array) {
+            if (el < min) {
+                min = el;
+            }
+        }
+        return min;
+    }
+
+
+    private static void delimiters() {
+        System.out.println("Hello World again");
+
+        String[] splitStrings = "Hello World again".split(" ");
+        printText(splitStrings);
+
+        System.out.println("_".repeat(20));
+        printText("Hello", "World", "again");
+
+        String[] sArray = {"first", "second", "third", "fourth"};
+        System.out.println(String.join(",", sArray));
+    }
+
+    private static void printText(String... textList) {
+        for (String t : textList) {
+            System.out.println(t);
+        }
+    }
+
+    private static void arrayReferences() {
         int[] myIntArray = new int[5];
         int[] anotherArray = myIntArray;
 
