2453361d152c376f5391965bd3995538f15189fe
Kedjian <o.mat.makowski@gmail.com>
2025-03-13 16:54:51 +0100

    added finalArrayExercises method

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -8,6 +8,33 @@ public class Main {
 
     }
 
+    public static void finalArrayExercises() {
+        int[][] array = {{1, 2}, {11, 12, 13}, {22, 33, 44, 55}};
+        int[][] twoDimensionalArray = new int[3][3];
+        int[][] array2 = new int[4][4];
+        System.out.println(Arrays.toString(array2));
+        System.out.println("array2.length = " + array2.length);
+
+        for (int[] outer : array2) {
+            System.out.println(Arrays.toString(outer));
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            var innerArray = array2[i];
+            for (int j = 0; j < innerArray.length; j++) {
+                System.out.print(array2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (var outer : array2) {
+            for (var element : outer) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
     private static void findDuplicatesTest() {
         int[] arr = {1, 2, 2, 3, 4};
         findDuplicates(arr);
