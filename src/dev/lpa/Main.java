149c5891824a70b37da342da79a11df586458bbc
Kedjian <o.mat.makowski@gmail.com>
2025-03-18 21:38:32 +0100

    added arrayListToAnArrayAndArrayToAnArrayList method

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -15,6 +15,32 @@ record GroceryItem(String name, String type, int count) {
 
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static void arrayListToAnArrayAndArrayToAnArrayList() {
+        String[] my_array = new String[] { "A", "B", "C", "D", "E", "F", "G" };
+        ArrayList list = new ArrayList(Arrays.asList(my_array));
+        System.out.println(list);
+
+        ArrayList<String> listList = new ArrayList<String>();
+        listList.add("A");
+        listList.add("B");
+        listList.add("C");
+        listList.add("D");
+        listList.add("E");
+        listList.add("F");
+        listList.add("G");
+
+        String[] my_arrayMy_Array = new String[list.size()];
+        listList.toArray(my_arrayMy_Array);
+
+        for (String s : my_arrayMy_Array) {
+            System.out.println(s);
+        }
+    }
+
+    public static void mobilePhoneTest() {
         Contact Sarah = new Contact("Sarah", "742832132");
         Contact Bob = new Contact("Bob", "678921322");
         Contact Roksana = new Contact("Roksana", "678921322");
@@ -27,25 +53,27 @@ public class Main {
         MobilePhone Ref = new MobilePhone("521423224");
         Ref.addNewContact(Roksana);
         Ref.printContacts();
+    }
 
-//        GroceryItem[] groceryArray = new GroceryItem[3];
-//        groceryArray[0] = new GroceryItem("Milk");
-//        groceryArray[1] = new GroceryItem("Bread", "Produce", 6);
-//        groceryArray[2] = new GroceryItem("Cheese", "Produce", 5);
-//        System.out.println(Arrays.toString(groceryArray));
+    public static void groceryItemTest() {
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("Milk");
+        groceryArray[1] = new GroceryItem("Bread", "Produce", 6);
+        groceryArray[2] = new GroceryItem("Cheese", "Produce", 5);
+        System.out.println(Arrays.toString(groceryArray));
 
-//        ArrayList objectList = new ArrayList();
-//        objectList.add(new GroceryItem("Butter"));
-//        objectList.add("Yogurt");
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
 
-//        ArrayList<GroceryItem> groceryList = new ArrayList<>();
-//        groceryList.add(new GroceryItem("Butter"));
-//        groceryList.add(new GroceryItem("Yogurt"));
-//        groceryList.add(new GroceryItem("Milk", "PRODUCE", 5));
-//        groceryList.set(0,
-//                new GroceryItem("Apples", "PRODUCE", 6));
-//        groceryList.remove(1);
-//        System.out.println(groceryList);
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("Yogurt"));
+        groceryList.add(new GroceryItem("Milk", "PRODUCE", 5));
+        groceryList.set(0,
+                new GroceryItem("Apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
     }
 
     public static void sayHelloToThePersonSeeingThis(String Name) {
