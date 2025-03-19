987a24f9acef4c2d3fb70eee6a46140567d2f4fd
Kedjian <o.mat.makowski@gmail.com>
2025-03-19 16:35:41 +0100

    added pairsToSpecifiedNumber method

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -15,8 +15,18 @@ record GroceryItem(String name, String type, int count) {
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 6, 7, -1, 8, 0};
-        checkIf(array);
+        int[] testArray = {1, 2, 3, 4, 5};
+        pairsToSpecifiedNumber(testArray, 5);
+    }
+
+    public static void pairsToSpecifiedNumber(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == number) {
+                    System.out.println(array[i] + " + " + array[j] + " = " + number);
+                }
+            }
+        }
     }
 
     public static void checkIf(int[] array) {
--- src/std/s34812.java
+++ src/std/s34812.java
@@ -2,38 +2,6 @@ package std;
 
 public class s34812 {
     public static void main(String[] args) {
-        int decimalValue = 165;
-        int secondDecimalValue = 12;
 
-        System.out.println("ĆW 2.1");
-
-        String binaryValue = Integer.toBinaryString(decimalValue);
-        String octalValue = Integer.toOctalString(decimalValue);
-        String hexValue = Integer.toHexString(decimalValue);
-
-        String secondBinaryValue = Integer.toBinaryString(secondDecimalValue);
-        String secondOctalValue = Integer.toOctalString(secondDecimalValue);
-        String secondHexValue = Integer.toHexString(secondDecimalValue);
-
-        System.out.println("Binary: " + binaryValue);
-        System.out.println("Octal: " + octalValue);
-        System.out.println("Hex: " + hexValue);
-
-        System.out.println("Second Binary: " + secondBinaryValue);
-        System.out.println("Second Octal: " + secondOctalValue);
-        System.out.println("Second Hex: " + secondHexValue);
-
-        System.out.println();
-        System.out.println("ĆW 2.2");
-
-        int value = 14;
-
-        System.out.println("After shifting 0 positions to the right: " + value + ".");
-
-        int resultLeftTwo = value << 2;
-        System.out.println("After shifting 2 positions to the left: " + resultLeftTwo + ".");
-
-        int resultRightFour = resultLeftTwo >> 4;
-        System.out.println("After shifting 4 positions to the right: " + resultRightFour + ".");
     }
 }
