a0bea1344accafc11a5b3b5d4b894ab7253b0380
Kedjian <o.mat.makowski@gmail.com>
2025-03-23 20:54:03 +0100

    studying variables

--- src/std/s34812.java
+++ src/std/s34812.java
@@ -1,30 +1,8 @@
 package std;
 
+import java.util.Scanner;
+
 public class s34812 {
     public static void main(String[] args) {
-        // standard notation of a numeric literal
-        System.out.println(10);
-        // octal notation
-        System.out.println(010);
-        // hexadecimal notation
-        System.out.println(0x10);
-        // binary notation
-        System.out.println(0b1010);
-
-        // bitwise negation
-        // 0 to 1; 1 to 0
-        System.out.println(~0b0101);
-
-        // bitwise AND
-        // 1 & 1 = 1; 0 & 1 = 0; 1 & 0 = 0; 0 & 0 = 0
-        System.out.println(0b1100 & 0b1010);
-
-        // bitwise OR
-        // 1 | 1 = 1; 0 | 1 = 1; 1 | 0 = 1; 0 | 0 = 0
-        System.out.println(0b1100 | 0b1010);
-
-        // bitwise XOR
-        // 1 ^ 1 = 0; 1 ^ 0 = 1; 0 ^ 1 = 1; 0 ^ 0 = 0
-        System.out.println(0b1100 ^ 0b1010);
     }
 }
