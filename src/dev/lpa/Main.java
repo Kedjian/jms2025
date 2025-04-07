79c5a2cdbafab05b864a6cf00577b182149546c4
Kedjian <o.mat.makowski@gmail.com>
2025-04-07 13:48:07 +0200

    studying autoboxing

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -14,7 +14,46 @@ record GroceryItem(String name, String type, int count) {
 
 public class Main {
     public static void main(String[] args) {
+        Integer boxedInt = Integer.valueOf(15); // preferred but unnecessary
+        /// Integer deprecatedBoxing = new Integer(15); deprecated
+        int unboxedInt = boxedInt.intValue();
 
+        // automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+//        System.out.println(autoUnboxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+
+        System.out.println(resultBoxed);
+        System.out.println(resultUnboxed);
+
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+
+        System.out.println(wrapperArray[0].getClass().getName());
+
+        Character[] characterArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+    }
+
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+
+    private static Integer returnAnInteger(int i) {
+        return i;
+    }
+
+    private static Double getDoubleObject() {
+        return Double.valueOf(100.00);
+    }
+
+    private static double getLiteralDoublePrimitive() {
+        return 100.0;
     }
 
     private static void PlaylistCreator() {
