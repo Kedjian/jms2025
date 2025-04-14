6e5e07ccc4c983768d819b9d0ecd99c5233294f8
Kedjian <o.mat.makowski@gmail.com>
2025-04-14 17:38:13 +0200

    studying enums further

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,6 @@
 package dev.lpa;
 import java.util.*;
 
-
 record GroceryItem(String name, String type, int count) {
     public GroceryItem(String name) {
         this(name, "DAIRY", 1);
@@ -15,7 +14,25 @@ record GroceryItem(String name, String type, int count) {
 
 public class Main {
     public static void main(String[] args) {
+        DayOfTheWeek weekDay = DayOfTheWeek.TUES;
+        System.out.println(weekDay);
 
+        for (int i = 0; i < 10; i++) {
+            weekDay = getRandomDay();
+            System.out.printf("Name is %s, Ordinal Value = %d%n",
+                    weekDay.name(), weekDay.ordinal());
+
+            if (weekDay == DayOfTheWeek.FRI) {
+                System.out.println("Found a Friday!!!");
+            }
+        }
+    }
+
+    public static DayOfTheWeek getRandomDay() {
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
     }
 
     private static void bankApplicationTest() {
