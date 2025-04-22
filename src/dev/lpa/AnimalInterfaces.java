8cf437e52f43bc50f79b404d1fc4cc8d4daba8e1
Kedjian <o.mat.makowski@gmail.com>
2025-04-22 09:56:39 +0200

    studying interfaces

--- src/dev/lpa/AnimalInterfaces.java
+++ src/dev/lpa/AnimalInterfaces.java
@@ -1,6 +1,11 @@
 package dev.lpa;
 
 interface FlightEnabled {
+    double MILES_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
+    /// public final double MILES_TO_KIM = 1.60934;
+    /// public static final double MILES_TO_KIM = 1.60934;
+    /// they're all the same
     public abstract void takeOff();
     abstract void land();
     void fly();
