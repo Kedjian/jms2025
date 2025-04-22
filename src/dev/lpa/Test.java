7ee234815bafc9d70be2e33e129e2afa0c99bcb2
Kedjian <o.mat.makowski@gmail.com>
2025-04-22 10:49:35 +0200

    keep on studying interfaces

--- src/dev/lpa/Test.java
+++ src/dev/lpa/Test.java
@@ -4,6 +4,9 @@ public class Test {
     public static void main(String[] args) {
         JetInterface jettyJet = new JetInterface();
         jettyJet.transition(FlightStages.LAUNCH);
+
+//        OrbitEarth.log("Testing" + new Satellite());
+        orbit(new Satellite());
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -12,6 +15,12 @@ public class Test {
         if (flier instanceof Trackable tracked) {
             tracked.track();
         }
+        flier.land();
+    }
+
+    private static void orbit(OrbitEarth flier) {
+        flier.takeOff();
+        flier.fly();
         flier.land();
     }
 }
