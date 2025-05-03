4d0e8da49acacc5f85e3f252a18facde3b8b1ff0
Kedjian <o.mat.makowski@gmail.com>
2025-05-03 13:44:34 +0200

    studying Comparable interface and techniques of sorting non primitive structures

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -22,7 +22,36 @@ record GolfPlayer(String name, String position) implements Player {}
 
 public class Main {
     public static void main(String[] args) {
+        Integer five = 5;
+        Integer[] others = {0, 5, 10, -50, 50};
 
+        for (Integer i : others) {
+            int val = five.compareTo(i);
+            System.out.printf("%d %s %d: compareTo=%d%n", five,
+                    (val == 0 ? "==" : (val < 0) ? "<"  : ">"), i, val);
+        }
+
+        String banana = "banana";
+        String[] fruit = {"apple", "banana", "pear", "BANANA"};
+
+        for (String s : fruit) {
+            int val = banana.compareTo(s);
+            System.out.printf("%s %s %s: compareTo=%d%n", banana,
+                    (val == 0 ? "==" : (val < 0) ? "<" : ">"), s, val);
+        }
+
+        Arrays.sort(fruit);
+        System.out.println(Arrays.toString(fruit));
+
+        System.out.println("A"+(int)'A' + " " + "a:"+(int)'a');
+        System.out.println("B"+(int)'B' + " " + "b:"+(int)'b');
+        System.out.println("C"+(int)'C' + " " + "c:"+(int)'c');
+
+        Student tim = new Student("Tim");
+        Student [] students = {new Student("Zach"), new Student("Tim"), new Student("Ann")};
+
+        Arrays.sort(students);
+        System.out.println(Arrays.toString(students));
     }
 
     public static void genericsExercise9() {
@@ -197,5 +226,24 @@ public class Main {
         String message = team1.setScore(t1_score, t2_score);
         team2.setScore(t2_score, t1_score);
         System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+}
+
+class Student implements Comparable {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Student other = (Student) o;
+        return name.compareTo(other.name);
     }
 }
