0bb41ecfe764b7dd6864a0c588800c1816226626
Kedjian <o.mat.makowski@gmail.com>
2025-05-03 18:45:28 +0200

    studying nested classes

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,6 +2,7 @@ package dev.lpa;
 
 import dev.lpa.domain.Employee;
 import dev.lpa.domain.EmployeeComparator;
+import dev.lpa.domain.StoreEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,21 @@ public class Main {
         employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
 
         for (Employee e : employees) {
+            System.out.println(e);
+        }
+
+        System.out.println("Store Members");
+
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Caroline", 2013, "Walmart "),
+                new StoreEmployee(10016, "David", 2007, "Frog Store "),
+                new StoreEmployee(10017, "Gordon", 2005, "Pokemon Center "),
+                new StoreEmployee(10019, "Skipping numbers, huh?", 2001, "Malicious Plushies Store ")
+        ));
+
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+        for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
         }
     }
