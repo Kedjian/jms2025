c4067e5b93e206ddf678cb22d671b0231208d2c4
Kedjian <o.mat.makowski@gmail.com>
2025-05-03 13:26:22 +0200

    added genericsExercise9 method

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -22,7 +22,29 @@ record GolfPlayer(String name, String position) implements Player {}
 
 public class Main {
     public static void main(String[] args) {
-        genericsExercise8();
+
+    }
+
+    public static void genericsExercise9() {
+        var nationalUSParks = new Park[] {
+                new Park("Yellowstone", "44.4882, -110.5916"),
+                new Park("Grand Canyon", "36.1085, -112.0965"),
+                new Park("Yosemite", "37.8855, -119.5360")
+        };
+
+        Layer<Park> parkLayer = new Layer<>(nationalUSParks);
+        parkLayer.renderLayer();
+
+        var majorUSRivers = new River[]{
+                new River("Mississippi",
+                        "47.2160, -95.2348", "29.1566, -89.2495",
+                        "35.1556, -90.0659"),
+                new River("Missouri", "45.9239, -111.4983," +
+                        "38.8146, -90.1218")
+        };
+
+        Layer<River> riverLayer = new Layer<>(majorUSRivers);
+        riverLayer.renderLayer();
     }
 
     public static void genericsExercise8() {
